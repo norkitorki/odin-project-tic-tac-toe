588137c34597c10af790a5d6b5fe3be747bffaf8
norkitorki <norkitorki@tutanota.de>
2021-03-23 16:33:03 +0100

    add dummy game board template

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,9 +1,14 @@
 require_relative '../lib/tic_tac_toe'
 
 describe TicTacToe do
+  game_board_template = GameBoard
   let(:tic_tac_toe) {
-    TicTacToe.new(GameBoard, Player.new('John', 'X'), Player.new('Jane', 'O'))
+    TicTacToe.new(
+      game_board_template,
+      Player.new('John', 'X'), Player.new('Jane', 'O')
+    )
   }
+  let(:game_board_dummy) { game_board_template.new(ranks: 3, files: 3) }
 
   it 'should print the current turn to the console' do
     expect(tic_tac_toe.to_s).to eq(
@@ -11,8 +16,8 @@ describe TicTacToe do
 
       John's turn.
 
-      #{tic_tac_toe.board}
-      Available fields:       A1, A2, A3, B1, B2, B3, C1, C2, C3
+      #{game_board_dummy}
+      Available fields:     A1, A2, A3, B1, B2, B3, C1, C2, C3
 
     GAME
     )
