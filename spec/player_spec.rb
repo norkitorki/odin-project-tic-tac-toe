57f33f74e95358fd6fb8fd1867d9e11af853c672
norkitorki <norkitorki@tutanota.de>
2021-03-24 11:27:57 +0100

    add setter methods tests

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -18,4 +18,14 @@ describe Player do
   it 'should return the piece' do
     expect(player.piece).to eq('X')
   end
+
+  it 'should change the name' do
+    player.name = 'Jonathan'
+    expect(player.name).to eq('Jonathan')
+  end
+
+  it 'should change the piece' do
+    player.piece = 'J'
+    expect(player.piece).to eq('J')
+  end
 end
